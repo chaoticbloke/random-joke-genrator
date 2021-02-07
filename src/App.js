04fc43edca118a506c82adc9885ebd8f4e12d340
aditya <adityagautamm@rediffmail.com>
2021-02-07 16:06:13 +0530

    random joke

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
 function App() {
+  const url = "https://official-joke-api.appspot.com/random_ten";
+  const [data, setData] = useState([]);
+  const [jokes, setJokes] = useState([]);
+  const [count, setCount] = useState(0);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("inside handle submit");
+  };
+
+  const fetchJokes = async (url) => {
+    const response = await fetch(url);
+    const finalResponse = await response.json();
+    setData(finalResponse);
+  };
+  useEffect(() => {
+    fetchJokes(url);
+  }, []);
+  const generatePara = (num) => {
+    if (count < 0) num = 0;
+    if (count > data.length) num = data.length;
+    setJokes(data.slice(0, num));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="section-center">
+        <h2>Random Joke Generator</h2>
+
+        <form onSubmit={handleSubmit} className="lorem-form">
+          <label htmlFor="number">How many?</label>
+          <input
+            type="number"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button className="btn" onClick={() => generatePara(count)}>
+            Generate
+          </button>
+        </form>
+
+        <article className="article">
+          {jokes.map((item) => {
+            const { setup, punchline, id } = item;
+            return (
+              <div key={id}>
+                <p>{setup}</p>
+                <p>{punchline}</p>
+                <hr />
+              </div>
+            );
+          })}
+        </article>
+      </section>
+    </main>
   );
 }
 
